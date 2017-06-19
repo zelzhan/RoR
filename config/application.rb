c40ad40fa035dd107f88b0c4f5bd43023480d5c7
zelzhan <elzhan.zeinulla@nu.edu.kz>
2017-06-19 19:28:28 +0600

    first commit

---
+++ config/application.rb
@@ -0,0 +1,24 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+require	File.expand_path('../boot',	__FILE__)
+
+module	Instagram
+		class	Application	<	Rails::Application
+
+				config.assets.precompile	+=	%w(*.png	*.jpg	*.jpeg	*.gif)
+		end
+end
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Demo3
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
